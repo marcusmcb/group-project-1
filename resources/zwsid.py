786c758be93b951be0bbad56d8ae305b03fae05a
roopak5 <32911253+roopak5@users.noreply.github.com>
2019-04-08 23:36:00 -0700

    Add files via upload

---
+++ resources/zwsid.py
@@ -0,0 +1,6 @@
+#api_key = "X1-ZWz1gzv7xiu0p7_7kib9"
+#estated_key = "Qtt2miJF6V8DQO4ApwFFVF1hoPVsyP"
+
+# https://estated.com/developers/usage
+
+google_api = "AIzaSyAiTe8J6__0Ajnw58_W6CPl6vtH5-TpuE4"
